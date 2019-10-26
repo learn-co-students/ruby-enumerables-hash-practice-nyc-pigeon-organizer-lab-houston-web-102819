bd482d13d5d1268d9b1aff4416a21f6ac2d9d9a0
David Jaworski <jaws360@gmail.com>
2019-10-26 04:08:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_list = {}
+    data.each do |c_g_l, value|
+      value.each do |traits, pigeon_names|
+        pigeon_names.each do |name|
+          if pigeon_list[name] == nil
+            pigeon_list[name] = {}
+          end
+          if pigeon_list[name][c_g_l] == nil
+            pigeon_list[name][c_g_l] = []
+          end
+          pigeon_list[name][c_g_l].push(traits.to_s)
+        end
+      end
+    end
+    pigeon_list
+  end
+            
+nyc_pigeon_organizer(pigeon_data)
